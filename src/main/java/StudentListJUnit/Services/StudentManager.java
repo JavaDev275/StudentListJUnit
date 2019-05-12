28c5ff7e995e19378c774414cd0996118b12e605
JavaDev275 <michael.rossiytsev@azimuth-grp.com>
2019-05-12 16:35:13 -0700

    Update-4: Complete Test Coverage

--- src/main/java/StudentListJUnit/Services/StudentManager.java
+++ src/main/java/StudentListJUnit/Services/StudentManager.java
@@ -1,23 +1,40 @@
 package StudentListJUnit.Services;
 
+import java.util.Random;
+import java.util.Arrays;
+
 public class StudentManager
 {
     private StudentStorage storage;
+    private Random rand;
+    private final String studentEntryDelimiter = ",";
+    private String studentList;
+    public String[] students;
 
     public StudentManager(StudentStorage storage)
     {
         this.storage = storage;
+        this.rand = new Random();
+        studentList = storage.loadStudentList();
+        students = studentList.split(studentEntryDelimiter);
     }
 
-    public String[] getAllStudents()
+    public String pickRandomStudent()
     {
-        String studentList = storage.LoadStudentList();
-        return studentList.split(",");
+        String[] students = studentList.split(studentEntryDelimiter);
+        int randomIndex = rand.nextInt(students.length);
+        return students[randomIndex];
     }
 
-    public int CountStudents()
+    public void addStudent(String newStudent)
     {
-        String studentList = storage.LoadStudentList();
-        return studentList.split(",").length;
+        studentList += studentEntryDelimiter + newStudent;
+        storage.updateStudentList(studentList);
+    }
+
+    public boolean studentExists(String student)
+    {
+        String[] students = studentList.split(studentEntryDelimiter);
+        return Arrays.asList(students).contains(student);
     }
 }
--- src/test/java/StudentListJUnit/studentManager_Should.java
+++ src/test/java/StudentListJUnit/studentManager_Should.java
@@ -6,15 +6,15 @@ import StudentListJUnit.Services.*;
 import org.mockito.Mockito;
 import java.util.*;
 
-public class studentManager_Should
+public class StudentManager_Should
 {
     StudentStorage mockStorage;
 
-    public studentManager_Should()
+    public StudentManager_Should()
     {
         mockStorage = Mockito.mock(StudentStorage.class);
         Mockito.when(
-            mockStorage.LoadStudentList()).thenReturn(
+            mockStorage.loadStudentList()).thenReturn(
                 "student1,student2,student3,student4"
             );
     }
@@ -26,7 +26,7 @@ public class studentManager_Should
         StudentManager sut = new StudentManager(mockStorage);
 
         // Act
-        String[] actual = sut.getAllStudents();
+        String[] actual = sut.students;
 
         // Assert
         Assert.assertEquals(String[].class, actual.getClass());
@@ -41,9 +41,67 @@ public class studentManager_Should
         StudentManager sut = new StudentManager(mockStorage);
 
         // Act
-        int actual = sut.CountStudents();
+        int actual = sut.students.length;
 
         // Assert
         Assert.assertEquals(4, actual);
+    }
+
+    @Test
+    public void returnRandomStudent()
+    {
+        // Arrange
+        StudentManager sut = new StudentManager(mockStorage);
+        String actualString = mockStorage.loadStudentList();
+
+        // Act
+        String expectedSubString = sut.pickRandomStudent();
+
+        // Assert
+        Assert.assertTrue(actualString.contains(expectedSubString));
+    }
+
+    @Test
+    public void call_UpdateStudentList_When_StudentAdded()
+    {
+        // Arrange
+        StudentManager sut = new StudentManager(mockStorage);
+        String originalList = mockStorage.loadStudentList();
+        String newStudent = "testStudent";
+        String updatedList = originalList + "," + newStudent;
+
+        // Act
+        sut.addStudent(newStudent);
+
+        // Assert
+        Mockito.verify(mockStorage).updateStudentList(updatedList);
+    }
+
+    @Test
+    public void returnTrue_When_SearchForExistingStudent()
+    {
+        // Arrange
+        StudentManager sut = new StudentManager(mockStorage);
+        String existingStudent = "student1";
+
+        // Act
+        Boolean actual = sut.studentExists(existingStudent);
+
+        // Assert
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void returnFalse_When_SearchForNonExistentStudent()
+    {
+        // Arrange
+        StudentManager sut = new StudentManager(mockStorage);
+        String fakeStudent = "fakeStudent";
+
+        // Act
+        Boolean actual = sut.studentExists(fakeStudent);
+
+        // Assert
+        Assert.assertFalse(actual);
     }
 }
